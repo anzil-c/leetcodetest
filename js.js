cf680647c67fe9a0d9d26f37accd375471cc8ac3
Anzil <anzilc008@gmail.com>
2025-04-10 17:17:32 +0530

    10/04/2025

---
+++ js.js
@@ -0,0 +1,13 @@
+let nums   = [2,7,11,15]
+let target = 9
+   for(let i=1;i<nums.length;i++){
+   if(nums[i]===9) }
+   console.log(nums)
+ 
+   
+   // for(const key in nums){
+//     if( for(
+//         let i =1;i<=target;i++
+//     )){
+//     }
+// }echo "# leetcodetest" >> README.md
